ad2def395a551a3c495670286456bbe0feb4cc44
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-21 23:02:05 +0600

    installed cors

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 const Vigenere = require('caesar-salad').Vigenere;
 
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/encode', (request, response) => {
