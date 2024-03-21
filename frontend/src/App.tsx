6c4675a95f52874e931c6c378bb1beeb021d8c53
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-21 23:03:11 +0600

    wrote routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import Vigenere from './components/Vigenere/Vigenere';
+import AppToolbar from './components/AppToolbar/AppToolbar';
+import { Container, Typography } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <div>Hello, world</div>
+    <>
+      <header>
+        <AppToolbar />
+      </header>
+      <main>
+        <Container maxWidth={"xl"}>
+          <Routes>
+            <Route path="/" element={<Vigenere />}/>
+            <Route path="*" element={<Typography variant="h1">Not found!</Typography>}/>
+          </Routes>
+        </Container>
+      </main>
+    </>
   );
 }
 
