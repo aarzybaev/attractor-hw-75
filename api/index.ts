055c93ab038c78895c67c5b8fc435b1a84a48cbb
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-22 11:08:29 +0600

    used types for express, req/res wrapped by try-catch

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import cors from 'cors';
 const Vigenere = require('caesar-salad').Vigenere;
 
@@ -9,18 +9,26 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/encode', (request, response) => {
-  const password = request.body.password;
-  const message = request.body.message;
-  const encoded = Vigenere.Cipher(password).crypt(message);
-  response.json({encoded});
+app.post('/encode', (request: Request, response: Response) => {
+  try {
+    const password: string = request.body.password;
+    const message: string = request.body.message;
+    const encoded: string = Vigenere.Cipher(password).crypt(message);
+    response.json({encoded});
+  } catch (e) {
+    console.error(e);
+  }
 });
 
-app.post('/decode', (request, response) => {
-  const password = request.body.password;
-  const message = request.body.message;
-  const decoded = Vigenere.Decipher(password).crypt(message);
-  response.json({decoded});
+app.post('/decode', (request: Request, response: Response) => {
+  try {
+    const password: string = request.body.password;
+    const message: string = request.body.message;
+    const decoded: string = Vigenere.Decipher(password).crypt(message);
+    response.json({decoded});
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(port, () => {
